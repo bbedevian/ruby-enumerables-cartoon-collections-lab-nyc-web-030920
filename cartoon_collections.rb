8327ec8d46c42ba587ff2e9fe797c68806685612
Brett Bedevian <brettbedevian@gmail.com>
2020-02-24 20:04:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,5 +21,6 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
  if  cheese_types.any? { |word| array.include?(word) }
- array.find {
+ array.find {|n| n == "cheddar"}
+ end 
 end
